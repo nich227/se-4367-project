fd0ab7e169f3ea68e9cfabe529d1575e5832418d
Kevin Chen <ningchen227@gmail.com>
2019-10-19 17:32:16 -0500

    Start fresh with java-agent

--- java-agent/agent/src/main/java/agent/Agent.java
+++
@@ -1,7 +0,0 @@
-package agent;
-
-public class Agent {
-    public static void premain(String agentArgs, Instrumentation inst) {
-        inst.addTransformer(new ClassTransformer());
-    }
-}
--- java-agent/agent/src/main/java/agent/JUnitExecutionListener.java
+++
@@ -1,8 +0,0 @@
-package agent;
-
-public class JUnitExecutionListener extends RunListener {
-    public void testRunStarted(Description desc) throws Exception {
-
-    }
-
-}
