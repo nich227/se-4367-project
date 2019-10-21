05f3ccb63ea5fc6e7920f03a79d57da817dd9d3e
Kevin Chen <ningchen227@gmail.com>
2019-10-21 12:42:06 -0500

    Getters and setters

--- agent/src/main/java/agent/CollectCoverage.java
+++ agent/src/main/java/agent/CollectCoverage.java
@@ -6,8 +6,12 @@ import it.unimi.dsi.fastutil.ints.IntSet;
 
 public class CollectCoverage {
 	
-	HashMap<String, IntSet> linesCovered;
-	HashMap<String, HashMap<String, IntSet>> testCases;
+	private HashMap<String, IntSet> linesCovered;
+	private HashMap<String, HashMap<String, IntSet>> testCases;
+	
+	//Accessors
+	public HashMap<String, IntSet> getLinesCovered() {return linesCovered;}
+	public HashMap<String, HashMap<String, IntSet>> getTestCases() {return testCases;}
 	
 	public static void addCoveredLine(String cName, Integer line) {
 		System.out.println(cName + "\t" + line);
