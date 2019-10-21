b82581339e2ffb6d95470d5bb665f31affee61aa
Kevin Chen <ningchen227@gmail.com>
2019-10-21 12:37:13 -0500

    Initialized coverage collector class

--- agent/src/main/java/agent/CollectCoverage.java
+++ agent/src/main/java/agent/CollectCoverage.java
@@ -1,5 +1,15 @@
 package agent;
 
-public class CollectCoverage {
+import java.util.HashMap;
 
+import it.unimi.dsi.fastutil.ints.IntSet;
+
+public class CollectCoverage {
+	
+	HashMap<String, IntSet> linesCovered;
+	HashMap<String, HashMap<String, IntSet>> testCases;
+	
+	public static void addCoveredLine(String cName, Integer line) {
+		System.out.println(cName + "\t" + line);
+	}
 }
