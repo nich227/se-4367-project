881cf0c5dcf7adf7a668e37332dd0c1e00470a42
Kevin Chen <ningchen227@gmail.com>
2019-10-21 15:07:44 -0500

    Mostly finished coverage collector

--- agent/src/main/java/agent/CollectCoverage.java
+++ agent/src/main/java/agent/CollectCoverage.java
@@ -2,18 +2,44 @@ package agent;
 
 import java.util.HashMap;
 
+import it.unimi.dsi.fastutil.ints.IntLinkedOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntSet;
 
 public class CollectCoverage {
 	
-	private HashMap<String, IntSet> linesCovered;
-	private HashMap<String, HashMap<String, IntSet>> testCases;
+	private static HashMap<String, IntSet> linesCovered;
+	private static HashMap<String, HashMap<String, IntSet>> testCases;
+	private static String testName;
+	
+	//Constructors
+	public CollectCoverage() {
+		linesCovered = new HashMap<String, IntSet>();
+		testCases = new HashMap<String, HashMap<String, IntSet>>();
+		testName = "";
+	}
 	
 	//Accessors
 	public HashMap<String, IntSet> getLinesCovered() {return linesCovered;}
 	public HashMap<String, HashMap<String, IntSet>> getTestCases() {return testCases;}
+	public String getTestName() {return testName;}
 	
-	public static void addCoveredLine(String cName, Integer line) {
-		System.out.println(cName + "\t" + line);
+	//Mutators
+	public void setLinesCovered(HashMap<String, IntSet> lc) {linesCovered = lc;}
+	public void setTestCases(HashMap<String, HashMap<String, IntSet>> tc) {testCases = tc;}
+	public void setTestName(String tn) {testName = tn;}
+	
+	
+	public static void addCoveredLine(String name, Integer line) {
+		
+		//If linesCovered has no values for the specified class name
+		if(linesCovered.get(name) == null) {
+			int[] new_set = {line};
+			linesCovered.put(name, new IntLinkedOpenHashSet(new_set));
+			return;
+		}
+		
+		//If adding lines to existing linesCovered
+		linesCovered.get(name).add(line);
+		
 	}
 }
