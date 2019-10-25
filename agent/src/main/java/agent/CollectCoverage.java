30250d3190b093c3279c1c54fe6c5b03d9fb9a43
Kevin Chen <ningchen227@gmail.com>
2019-10-25 18:58:04 -0500

    Trying to fix HashMap

--- agent/src/main/java/agent/CollectCoverage.java
+++ agent/src/main/java/agent/CollectCoverage.java
@@ -1,52 +1,48 @@
 package agent;
 
 import java.util.HashMap;
-import it.unimi.dsi.fastutil.ints.IntSet;
-import it.unimi.dsi.fastutil.ints.IntOpenHashSet;
 import it.unimi.dsi.fastutil.ints.IntLinkedOpenHashSet;
 
 public class CollectCoverage {
-	
+
 	public static HashMap<String, IntLinkedOpenHashSet> linesCovered;
 	public static HashMap<String, HashMap<String, IntLinkedOpenHashSet>> testCases;
 	public static String testName;
-	
-	//Constructors
+
+	// Constructors
 	public CollectCoverage() {
 		linesCovered = new HashMap<String, IntLinkedOpenHashSet>();
 		testCases = new HashMap<String, HashMap<String, IntLinkedOpenHashSet>>();
 		testName = "";
 	}
 
-	//Accessors
+	// Accessors
 
-	
-	
 	public static void addCoveredLine(String name, Integer line) {
-		
-		//The lines covered is empty
-		if(linesCovered == null || linesCovered.isEmpty() == true){
 
-		System.out.println("This is the line " + name +"| "+ line);
+		// The lines covered is empty
+		if (linesCovered == null || linesCovered.isEmpty() == true) {
 
-	    }
+			System.out.println("This is the line " + name + "| " + line);
+			return;
 
-	   IntLinkedOpenHashSet covered_lines = linesCovered.get(name);
-		//If linesCovered has no values for the specified class name
-		if(covered_lines == null) {
-			int[] new_set = {line};
-			System.out.println("This is the line " + name +"| "+ line);
-			linesCovered.put(name, new IntLinkedOpenHashSet(new_set));
-			//System.out.println("Putting line number" + line + "in" + name);
 		}
-		
-		else{
-		//If adding lines to existing linesCovered
-		covered_lines = new IntLinkedOpenHashSet(new int[]{line});
-			System.out.println("This is the line " + name +"| "+ line);
-		   linesCovered.put(name, covered_lines);
-		} 
+
+		IntLinkedOpenHashSet covered_lines = linesCovered.get(name);
+		// If linesCovered has no values for the specified class name
+		if (covered_lines == null) {
+			int[] new_set = { line };
+			System.out.println("This is the line " + name + "| " + line);
+			linesCovered.put(name, new IntLinkedOpenHashSet(new_set));
+			// System.out.println("Putting line number" + line + "in" + name);
+		}
+
+		else {
+			// If adding lines to existing linesCovered
+			covered_lines = new IntLinkedOpenHashSet(new int[] { line });
+			System.out.println("This is the line " + name + "| " + line);
+			linesCovered.put(name, covered_lines);
+		}
 	}
-		
-	
+
 }
--- agent/src/main/java/agent/JUnitListener.java
+++ agent/src/main/java/agent/JUnitListener.java
@@ -1,10 +1,8 @@
 package agent;
 
 import it.unimi.dsi.fastutil.ints.IntLinkedOpenHashSet;
-import it.unimi.dsi.fastutil.ints.IntSet;
 import org.junit.runner.Description;
 import org.junit.runner.Result;
-import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
 
 import java.io.*;
@@ -13,54 +11,52 @@ import java.util.HashMap;
 
 public class JUnitListener extends RunListener {
 
-    public void testRunStarted(Description description) throws Exception {
+	public void testRunStarted(Description description) throws Exception {
 
-        if (null == CollectCoverage.testCases) {
-            CollectCoverage.testCases = new HashMap<String,HashMap<String, IntLinkedOpenHashSet>>();
-        }
+		if (CollectCoverage.testCases == null) {
+			CollectCoverage.testCases = new HashMap<String, HashMap<String, IntLinkedOpenHashSet>>();
+		}
+	}
 
-        // System.out.println("\n\nStart!");
-    }
+	public void testStarted(Description description) {
+		System.out.println("\n-----\nStart ... " + description.getMethodName());
+		CollectCoverage.testName = "[TEST]" + description.getClassName() + ":" + description.getMethodName();
+		CollectCoverage.linesCovered = new HashMap<String, IntLinkedOpenHashSet>();
+	}
 
-    public void testStarted(Description description) {
-        System.out.println("\n\n\nStarting .... " + description.getMethodName());
-        CollectCoverage.testName = "[TEST]" + description.getClassName() + ":" + description.getMethodName();
-        CollectCoverage.linesCovered = new HashMap<String, IntLinkedOpenHashSet>();
-    }
-    public void testFinished(Description description) {
-        System.out.println("Finished .... "+ description.getMethodName());
-        CollectCoverage.testCases.put(CollectCoverage.testName, CollectCoverage.linesCovered);
-    }
-    public void testRunFinished(Result result) throws IOException {
-        System.out.println("Done\n\n");
-        // Write to stmt-cov.txt
-        File fout = new File("stmt-cov.txt");
-        FileOutputStream fos = new FileOutputStream(fout);
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
+	public void testFinished(Description description) {
+		System.out.println("Finish ... " + description.getMethodName());
+		CollectCoverage.testCases.put(CollectCoverage.testName, CollectCoverage.linesCovered);
+	}
 
-        StringBuilder builder = new StringBuilder();
-        for (String testCaseName : CollectCoverage.testCases.keySet()) {
-            builder.append(testCaseName + "\n");
+	public void testRunFinished(Result result) throws IOException {
+		System.out.println("Done\n\n");
 
+		File fout = new File("stmt-cov.txt");
+		FileOutputStream fos = new FileOutputStream(fout);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 
+		StringBuilder builder = new StringBuilder();
+		for (String testCaseName : CollectCoverage.testCases.keySet()) {
+			System.out.println("Test case: " + testCaseName);
+			builder.append(testCaseName + "\n");
 
-            HashMap<String, IntLinkedOpenHashSet> caseCoverage =
-                    CollectCoverage.linesCovered;
-            System.out.println(Arrays.asList(caseCoverage));
+			HashMap<String, IntLinkedOpenHashSet> caseCoverage = CollectCoverage.testCases.get(testCaseName);
+			System.out.println(Arrays.asList(caseCoverage));
 
-            for (String className : caseCoverage.keySet()) {
-                int[] lines = caseCoverage.get(className).toIntArray();
+			for (String className : caseCoverage.keySet()) {
+				int[] lines = caseCoverage.get(className).toIntArray();
 
-                Arrays.sort(lines);
-                System.out.println(Arrays.toString(lines));
-                for (int i = 0; i < lines.length; i++) {
-                    builder.append(className + ":" + lines[i] + "\n");
-                }
-            }
-        }
+				Arrays.sort(lines);
+				System.out.println(Arrays.toString(lines));
+				for (int i = 0; i < lines.length; i++) {
+					builder.append(className + ":" + lines[i] + "\n");
+				}
+			}
+		}
 
-        bw.write(builder.toString());
-        bw.close();
-    }
+		bw.write(builder.toString());
+		bw.close();
+	}
 
 }
